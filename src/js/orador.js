fe2f997a4253164f4b064c7c21149c9ccb39629b
Casto Gil <casto782@gmail.com>
2023-12-15 02:25:43 -0300

    actualizacion

--- src/js/orador.js
+++ src/js/orador.js
@@ -1,11 +1,10 @@
 function validarFormulario() {
-  // Obtener los valores de los campos
   const nombre = document.getElementById("nombre").value;
   const apellido = document.getElementById("apellido").value;
   const email = document.getElementById("email").value;
   const tema = document.getElementById("tema").value;
 
-  // Validar si algún campo está vacío
+  cío;
   if (nombre === "" || apellido === "" || email === "" || tema === "") {
     alert(
       "Por favor, complete todos los campos antes de enviar el formulario."
@@ -13,21 +12,17 @@ function validarFormulario() {
     return false;
   }
 
-  // Si la validación es exitosa, llama a la función para guardar al orador
   guardarOrador();
 
-  // Evitar que el formulario se envíe automáticamente
   return false;
 }
 
 function guardarOrador() {
-  // Obtener datos del formulario
   const nombre = document.getElementById("nombre").value;
   const apellido = document.getElementById("apellido").value;
   const email = document.getElementById("email").value;
   const tema = document.getElementById("tema").value;
 
-  // Construir objeto con los datos
   const nuevoOrador = {
     nombre: nombre,
     apellido: apellido,
@@ -35,7 +30,6 @@ function guardarOrador() {
     tema: tema,
   };
 
-  // Realizar llamada al backend para guardar el orador
   fetch("http://localhost:8080/web-app/api/orador", {
     method: "POST",
     headers: {
@@ -65,12 +59,11 @@ function mostrarModal(mensaje) {
 }
 
 function mostrarOradores() {
-  // Realizar llamada al backend para obtener la lista de oradores
   fetch("http://localhost:8080/web-app/api/orador")
     .then((response) => response.json())
     .then((data) => {
       console.log("Lista de oradores:", data);
-      // Puedes actualizar la tabla de oradores o realizar otras acciones
+
       actualizarTablaOradores(data);
     })
     .catch((error) => {
@@ -79,13 +72,10 @@ function mostrarOradores() {
 }
 
 function actualizarTablaOradores(oradores) {
-  // Obtener referencia a la tabla
   const tabla = document.getElementById("tablaOradores");
 
-  // Limpiar la tabla
   tabla.innerHTML = "";
 
-  // Agregar encabezados
   const encabezados = ["ID", "Nombre", "Apellido", "Email", "Tema", "Acciones"];
   const encabezadoRow = tabla.insertRow(0);
   encabezados.forEach((encabezado, index) => {
@@ -93,11 +83,9 @@ function actualizarTablaOradores(oradores) {
     cell.textContent = encabezado;
   });
 
-  // Agregar filas con datos de oradores
   oradores.forEach((orador) => {
     const row = tabla.insertRow();
 
-    // Iterar sobre todas las propiedades del objeto Orador, excepto 'fechaAlta'
     Object.keys(orador).forEach((key) => {
       if (key !== "fechaAlta") {
         const cell = row.insertCell();
@@ -105,7 +93,6 @@ function actualizarTablaOradores(oradores) {
       }
     });
 
-    // Agregar botones de acciones (eliminar y editar)
     const accionesCell = row.insertCell();
 
     const btnEditar = document.createElement("button");
@@ -122,23 +109,19 @@ function actualizarTablaOradores(oradores) {
 }
 
 function eliminarOrador(id) {
-  // Mostrar el modal de confirmación
   const modalEliminar = new bootstrap.Modal(
     document.getElementById("miModalEliminar")
   );
   modalEliminar.show();
 
-  // Configurar el evento click para el botón "Eliminar"
   document.getElementById("botonEliminar").onclick = function () {
     fetch(`http://localhost:8080/web-app/api/orador?id=${id}`, {
       method: "DELETE",
     })
       .then((response) => {
         if (response.ok) {
-          // Mostrar modal con el mensaje
           mostrarModal(`Orador con ID ${id} eliminado exitosamente`);
 
-          // Actualizar la tabla después de la eliminación
           mostrarOradores();
         } else {
           console.error("Error al eliminar el orador:", response.statusText);
@@ -148,12 +131,10 @@ function eliminarOrador(id) {
         console.error("Error al eliminar el orador:", error);
       });
 
-    // Cerrar el modal de confirmación
     modalEliminar.hide();
   };
 }
 
-// Función para manejar la edición de un orador
 function editarOrador(id) {
   fetch(`http://localhost:8080/web-app/api/orador?id=${id}`)
     .then((response) => {
@@ -165,7 +146,6 @@ function editarOrador(id) {
       return response.json();
     })
     .then((oradores) => {
-      // Buscar el orador específico por ID
       const orador = oradores.find((o) => o.id === id);
 
       console.log("Orador seleccionado:", orador);
@@ -175,26 +155,22 @@ function editarOrador(id) {
         return;
       }
 
-      // Configurar campos de entrada con los datos del orador
       document.getElementById("nuevoNombre").value = orador.nombre || "";
       document.getElementById("nuevoApellido").value = orador.apellido || "";
       document.getElementById("nuevoEmail").value = orador.email || "";
       document.getElementById("nuevoTema").value = orador.tema || "";
 
-      // Mostrar el modal de edición
       const modalEditar = new bootstrap.Modal(
         document.getElementById("miModalEditar")
       );
       modalEditar.show();
 
-      // Configurar el evento click para el botón "Guardar Cambios"
       document.getElementById("botonGuardarEdicion").onclick = function () {
         const nuevoNombre = document.getElementById("nuevoNombre").value;
         const nuevoApellido = document.getElementById("nuevoApellido").value;
         const nuevoEmail = document.getElementById("nuevoEmail").value;
         const nuevoTema = document.getElementById("nuevoTema").value;
 
-        // Construir objeto con los datos actualizados
         const datosActualizados = {
           nombre: nuevoNombre,
           apellido: nuevoApellido,
@@ -202,7 +178,6 @@ function editarOrador(id) {
           tema: nuevoTema,
         };
 
-        // Realizar llamada al backend para actualizar los datos del orador
         fetch(`http://localhost:8080/web-app/api/orador?id=${id}`, {
           method: "PUT",
           headers: {
@@ -212,10 +187,8 @@ function editarOrador(id) {
         })
           .then((response) => {
             if (response.ok) {
-              // Mostrar modal con el mensaje
               mostrarModal(`Orador con ID ${id} editado exitosamente`);
 
-              // Actualizar la tabla después de la edición
               mostrarOradores();
             } else {
               console.error("Error al editar el orador:", response.statusText);
@@ -225,7 +198,6 @@ function editarOrador(id) {
             console.error("Error al editar el orador:", error);
           });
 
-        // Cerrar el modal de edición
         modalEditar.hide();
       };
     })
